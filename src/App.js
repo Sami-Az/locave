cd6e96b17e1d79664edef4aeb27f12cd1fef5e57
Sami Abz <sami.abderre@gmail.com>
2020-12-30 16:37:22 +0100

    add nav bar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
 import './App.css';
 import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/navigation/MainNavigation';
 import UsersPage from './users/pages/Users';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/">
-          <UsersPage />
-        </Route>
-        <Route exact path="/places/new" >
-          <NewPlace />
-        </Route>
-        <Redirect to="/"/>
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route exact path="/">
+            <UsersPage />
+          </Route>
+          <Route exact path="/places/new" >
+            <NewPlace />
+          </Route>
+          <Redirect to="/"/>
+        </Switch>
+      </main>
     </Router>
   );
 }
--- src/users/components/UsersList.js
+++ src/users/components/UsersList.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import Card from '../../shared/components/Card';
 import UserItem from './UserItem';
+import './UsersList.css';
 
 const UsersList = (props) => {
     if (props.items.length === 0) {
-        return <div>No Users found.</div>
+        return (
+            <div className="center">
+                <Card>
+                    <h2>No users found.</h2>
+                </Card>
+            </div>
+        )
     }
     return (
         <ul className="users-list">
