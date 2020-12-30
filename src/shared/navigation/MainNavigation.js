078fe92d5bfde5186d6013787a1cca3929cf3659
Sami Abz <sami.abderre@gmail.com>
2020-12-30 16:46:54 +0100

    add nav links

--- src/shared/navigation/MainNavigation.js
+++ src/shared/navigation/MainNavigation.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import MainHeader from './MainHeader';
 import './MainNavigation.css';
+import NavLinks from './NavLinks';
 
 const MainNavigation = (props) => {
     return (
@@ -15,7 +16,7 @@ const MainNavigation = (props) => {
              <h1 className="main-navigation__title">ZePlaces</h1>
           </Link>
           <nav>
-              ...
+              <NavLinks />
           </nav>
         </MainHeader>
     )
--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -4,7 +4,7 @@ import UsersList from '../components/UsersList';
 const UsersPage = () => {
     const USERS = [
         {
-            id: 'dezdze2d43',
+            id: 'u1',
             name: 'Sam Luiz',
             image: 'https://images.unsplash.com/flagged/photo-1573740144655-bbb6e88fb18a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8dXNlcnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
             places: 3
