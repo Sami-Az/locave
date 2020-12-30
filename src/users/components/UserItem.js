d8fd56700b99424c2025bf1cb8f7255a0a559b9a
Sami Abz <sami.abderre@gmail.com>
2020-12-30 15:54:50 +0100

    use <Card/>

--- src/users/components/UserItem.js
+++ src/users/components/UserItem.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import Avatar from '../../shared/components/Avatar';
 import { Link } from 'react-router-dom';
+import Avatar from '../../shared/components/Avatar';
+import Card from '../../shared/components/Card';
+
 import './UserItem.css';
 
 const UserItem = (props) => {
     return (
         <li className="user-item">
-            <div className="user-item__content">
+            <Card className="user-item__content">
                 <Link to={`/${props.id}/places`}>
                     <div className="user-item__image">
                         <Avatar image={props.image} alt={props.name}/>
@@ -16,7 +18,7 @@ const UserItem = (props) => {
                         <h3>{props.placesCount} {props.placesCount === 1 ? 'place' : 'places'}</h3>  
                     </div>
                 </Link>
-            </div> 
+            </Card>
         </li>
     )
 }
--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -8,6 +8,12 @@ const UsersPage = () => {
             name: 'Sam Luiz',
             image: 'https://images.unsplash.com/flagged/photo-1573740144655-bbb6e88fb18a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8dXNlcnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
             places: 3
+        },
+        {
+            id: 'dezdze2d43',
+            name: 'Sam Luiz',
+            image: 'https://images.unsplash.com/flagged/photo-1573740144655-bbb6e88fb18a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8dXNlcnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
+            places: 3
         }
     ];
     return (
