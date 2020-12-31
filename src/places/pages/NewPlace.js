b0d82eb70475e69c971956defd8a1476a135a207
Sami Abz <sami.abderre@gmail.com>
2020-12-31 20:27:37 +0100

    add place form custom input

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Input from '../../shared/components/FormElements/Input';
+
+import './NewPlace.css';
 
 const NewPlace = () => {
     return (
-        <div>
-           New place 
-        </div>
+        <form className="place-form">
+           <Input element="input" type="text" label="Title"/>
+        </form>
     )
 }
 
---
+++ src/shared/components/FormElements/Input.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import './Input.css';
+
+const Input = (props) => {
+    const element = props.element === 'input' ? (
+      <input
+        id={props.id}
+        type={props.type}
+        placeholder={props.placeholder}
+      />
+    ) : (
+      <textarea
+        id={props.id}
+      />
+    );
+
+    return (
+        <div className={`form-control`}>
+            <label htmlFor={props.id}>{props.label}</label>
+            {element}
+        </div>
+    )
+}
+
+export default Input;
