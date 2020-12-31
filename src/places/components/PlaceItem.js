768465a62e72fc0564d5a9ee503a9fc55bee8f65
Sami Abz <sami.abderre@gmail.com>
2020-12-31 17:46:38 +0100

    modal for map

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Button from '../../shared/components/FormElements/Button';
 import Card from '../../shared/components/UIElements/Card';
+import Modal from '../../shared/components/UIElements/Modal';
 import './PlaceItem.css';
 
 const PlaceItem = props => {
- 
+  const [showMap, setShowMap] = useState(false);
+
+  const openMapHandler = () => setShowMap(true);
+
+  const closeMapHandler = () => setShowMap(false);
+
   return (
+    <>
+      <Modal
+        show={showMap}
+        onCancel={closeMapHandler}
+        header={props.address}
+        contentClass="place-item__modal-content"
+        footerClass="place-item__modal-actions"
+        footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
+      >
+        <div className="map-container">
+          <h2>MAP HERE</h2>
+        </div>
+      </Modal>
       <li className="place-item">
         <Card className="place-item__content">
           <div className="place-item__image">
@@ -18,12 +37,13 @@ const PlaceItem = props => {
             <p>{props.description}</p>
           </div>
           <div className="place-item__actions">
-            <Button inverse>VIEW ON MAP</Button>
+            <Button inverse onClick={() => openMapHandler()}>VIEW ON MAP</Button>
             <Button>EDIT</Button>
             <Button danger>DELETE</Button>
           </div>
         </Card>
       </li>
+    </>
   );
 };
 
